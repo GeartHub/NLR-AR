89bbb49c1e49e2d6f94a9cbf28d2be0d27d4d3bd
Gearthub <geart.otten@gmail.com>
2020-06-15 14:47:01 +0200

    Added a fixNow bool to the damages

--- DamageNode+CoreDataProperties.swift
+++ DamageNode+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  DamageNode+CoreDataProperties.swift
 //  NLR AR
 //
-//  Created by Geart Otten on 13/06/2020.
+//  Created by Geart Otten on 15/06/2020.
 //  Copyright © 2020 Geart Otten. All rights reserved.
 //
 //
@@ -17,10 +17,11 @@ extension DamageNode {
         return NSFetchRequest<DamageNode>(entityName: "DamageNode")
     }
 
-    @NSManaged public var title: String?
-    @NSManaged public var coordinates: Coordinates?
-    @NSManaged public var aircraft: Set<Aircraft>?
     @NSManaged public var createdAt: Date?
+    @NSManaged public var title: String?
+    @NSManaged public var fixNow: Bool
+    @NSManaged public var aircraft: NSSet?
+    @NSManaged public var coordinates: Coordinates?
 
 }
 
