bc4295150e02b078af715d313657e5db0217fd6d
ruurdinho <ruurdsinnema@hotmail.com>
2020-06-15 13:26:25 +0200

    Deleted the UIEdgeInsets on the table to make the table go to the top of the view for more space and better scrolling functionality.

--- NLR AR/ReportSelectionViewController.swift
+++ NLR AR/ReportSelectionViewController.swift
@@ -84,7 +84,6 @@ class ReportSelectionViewController: UIViewController, UITableViewDelegate, UITa
         // Register the list
         planesTable.register(PlaneCell.self, forCellReuseIdentifier: planeCellId)
         self.planesTable.tableFooterView = UIView()
-        self.planesTable.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
         
         // Setup delegates
         planesTable.delegate = self
