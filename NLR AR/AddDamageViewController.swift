9de6b10a46f738d114d720b67c083df19a97f5e3
ruurdinho <ruurdsinnema@hotmail.com>
2020-06-16 15:12:41 +0200

    Added a uiview containing a label and a textfield to make a item for the add damage form.

--- NLR AR/AddDamageViewController.swift
+++ NLR AR/AddDamageViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class AddDamageViewController: UIViewController {
     
+    let descriptionContentView = UIView()
+    let descriptionLabel = UILabel()
     let descriptionField = UITextField(frame: CGRect(x: 10.0, y: 100.0, width: UIScreen.main.bounds.size.width, height: 50.0))
+    
+    let titleLabel = UILabel()
+    let titleField  = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,29 +24,44 @@ class AddDamageViewController: UIViewController {
 
         // Autoresizing is the automatic contraints of Apple, we dont want that.
         self.descriptionField.translatesAutoresizingMaskIntoConstraints = false
+        self.descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.titleField.translatesAutoresizingMaskIntoConstraints = false
+        self.descriptionField.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Description View
+        
         
         // Descriptionfield
-        descriptionField.backgroundColor = .yellow
+        self.descriptionField.backgroundColor = .systemGray6
+        self.descriptionField.placeholder = "Add a Description..."
+        self.descriptionField.isUserInteractionEnabled = true
+        
+        // Description label
+        self.descriptionLabel.text = "Description:"
+        self.descriptionLabel.textColor = .white
 
         // Adding items to the View
-        view.addSubview(descriptionField)
+        view.addSubview(descriptionContentView)
+        descriptionContentView.addSubview(descriptionField)
+        descriptionContentView.addSubview(descriptionLabel)
         
         setupConstraints()
-    }
-    
-    @objc func goToMainView() {
-        self.navigationController?.pushViewController(PlaneSelectionViewController(), animated: true)
-        
     }
     
     // Setup of the contraints for this viewcontroller. This is the placement of the items.
     private func setupConstraints() {
         // logoConstraints
         NSLayoutConstraint.activate([
-            descriptionField.leftAnchor.constraint(equalTo: view.leftAnchor),
-            descriptionField.rightAnchor.constraint(equalTo: view.rightAnchor),
-            descriptionField.topAnchor.constraint(equalTo: view.topAnchor, constant: 50)
+            descriptionContentView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            descriptionContentView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            descriptionField.leadingAnchor.constraint(equalTo: descriptionLabel.trailingAnchor)
+
         ])
+         NSLayoutConstraint.activate([
+            descriptionLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
+            descriptionLabel.widthAnchor.constraint(equalToConstant: 150)
+         ])
     }
     
     // Setting the status bar to dark
